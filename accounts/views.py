c25a6273276dab7ec1867120aa590df079de3a62
AnDrEy200010 <bazylevich2000@mail.ru>
2021-05-21 23:25:41 +0300

    new

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from shopping_cart.models import Order
 from .models import Profile
 from django.urls import reverse
 import pyperclip
+import subprocess
+import pandas
+
 
 def my_profile(request):
 	my_user_profile = Profile.objects.filter(user=request.user).first()
@@ -18,5 +21,7 @@ def my_profile(request):
 def copy_from_cart(request, item_id):
 	item_to_copy = OrderItem.objects.filter(pk=item_id)
 	if item_to_copy.exists():
-		pyperclip.copy(str(item_to_copy[0].product.number_track))
+		# pyperclip.copy(str(item_to_copy[0].product.number_track))
+		df = pandas.DataFrame([str(item_to_copy[0].product.number_track)])
+		df.to_clipboard(index=False, header=False)
 	return redirect(reverse('accounts:my_profile'))
